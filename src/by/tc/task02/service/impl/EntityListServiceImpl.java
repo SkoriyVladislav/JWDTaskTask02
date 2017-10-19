ff07524a2a9608df61e1adb10ceca9eabdca3ace
SkoriyVladislav <Skoriy.97@gmail.com>
2017-10-20 01:03:57 +0300

    EntityList 19.10

--- src/by/tc/task02/service/impl/EntityListServiceImpl.java
+++ src/by/tc/task02/service/impl/EntityListServiceImpl.java
@@ -8,8 +8,13 @@ import by.tc.task02.service.EntityListService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EntityListServiceImpl implements EntityListService {
+    static private String OPENTAG = "<[a-z]|[A-Z]>";
+    static private String CLOUSETAG = "</[a-z]|[A-Z]>";
+
     @Override
     public List<Entity> createListEntity() {
         DAOFactory factory = DAOFactory.getInstance();
@@ -23,9 +28,30 @@ public class EntityListServiceImpl implements EntityListService {
     }
 
     private void initializationList(List<Entity> list, Queue<String> queue) {
+        int level = 0;
         while (queue.peek() != null) {
-            String str = queue.poll();
+            String line = queue.poll();
 
+            if (Pattern.matches(OPENTAG, line)) {
+
+                Pattern pat = Pattern.compile(OPENTAG);
+                Matcher matcher = pat.matcher(line);
+                String str = null;
+                if (matcher.find())	{
+                    str = matcher.group().replaceAll("< | >", " ").trim();
+                }
+
+                list.add(new Entity(str, level));
+                level++;
+                continue;
+            }
+
+            if (Pattern.matches(CLOUSETAG, line)) {
+                level--;
+                continue;
+            }
+
+            list.get(list.size()).setValue(line);
         }
     }
 }
